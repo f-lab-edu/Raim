35563c2ce1562dd033fb0bf96192ea7122aaad37
ImWonYong <ssassb123@daum.net>
2023-10-06 02:07:51 +0900

    유저 레포지토리 생성

---
+++ src/main/java/flab/project/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package flab.project.repository;
+
+import flab.project.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+}
