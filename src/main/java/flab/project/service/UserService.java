f98dc1b2e0547c51f5eef5e4c560c7077a77567b
ImWonYong <ssassb123@daum.net>
2023-10-08 23:26:34 +0900

    - 이메일 인증 추가 - 회원가입 시 이메일과 sms 인증된 상태 확인 후 회원가입 하도록 변경

--- src/main/java/flab/project/service/UserService.java
+++ src/main/java/flab/project/service/UserService.java
@@ -1,21 +1,100 @@
 package flab.project.service;
 
+import flab.project.domain.EmailVerification;
+import flab.project.domain.SmsVerification;
 import flab.project.domain.User;
+import flab.project.repository.EmailVerificationRepository;
+import flab.project.repository.SmsVerificationRepository;
 import flab.project.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final EmailVerificationRepository emailVerificationRepository;
+    private final SmsVerificationRepository smsVerificationRepository;
 
-    public void registrationUser(User user) {
+    public boolean registrationUser(User user) {
+
+        LocalDateTime now = Instant.ofEpochMilli(System.currentTimeMillis()).atZone(ZoneId.systemDefault()).toLocalDateTime();
+
+        if (!checkEmailVerification(user.getEmail(), now) || !checkSmsVerification(user.getPhoneNumber(), now)) {
+            return false;
+        }
+
         userRepository.save(user);
+
+        return true;
+    }
+
+    private boolean checkEmailVerification(String email, LocalDateTime now) {
+        EmailVerification findEmailVerification = emailVerificationRepository.findByEmail(email)
+                .orElseThrow(() ->  new NoSuchElementException());
+
+        if (!findEmailVerification.checkExpiration(now) || !findEmailVerification.isVerified()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean checkSmsVerification(String phoneNumber, LocalDateTime now) {
+        SmsVerification findSmsVerification = smsVerificationRepository.findByPhoneNumber(phoneNumber)
+                .orElseThrow(() -> new NoSuchElementException());
+
+        if (!findSmsVerification.checkExpiration(now) || !findSmsVerification.isVerified()) {
+            return false;
+        }
+
+        return true;
     }
 
     public boolean duplicatedEmail(String email) {
-        return userRepository.existsByEmail(email);
+        if (userRepository.existsByEmail(email)) {
+            return true;
+        }
+
+        if (emailVerificationRepository.existsByEmail(email)) {
+            updateEmailVerification(email);
+        } else {
+            createEmailVerification(email);
+        }
+
+        return false;
+    }
+
+    private void updateEmailVerification(String email) {
+        EmailVerification findEmail = emailVerificationRepository.findByEmail(email).get();
+
+        Long milliseconds = System.currentTimeMillis();
+        LocalDateTime createdAt = Instant.ofEpochMilli(milliseconds).atZone(ZoneId.systemDefault()).toLocalDateTime();
+        LocalDateTime expirationTime = Instant.ofEpochMilli((milliseconds + 300000)).atZone(ZoneId.systemDefault()).toLocalDateTime();
+
+        findEmail.renewEmailVerification(createdAt, expirationTime);
+
+        emailVerificationRepository.save(findEmail);
+    }
+
+    private void createEmailVerification(String email) {
+        Long milliseconds = System.currentTimeMillis();
+        LocalDateTime createdAt = Instant.ofEpochMilli(milliseconds).atZone(ZoneId.systemDefault()).toLocalDateTime();
+        LocalDateTime expirationTime = Instant.ofEpochMilli((milliseconds + 300000)).atZone(ZoneId.systemDefault()).toLocalDateTime();
+
+        EmailVerification emailVerification = EmailVerification.builder()
+                .email(email)
+                .isVerified(true)
+                .createdAt(createdAt)
+                .expirationTime(expirationTime)
+                .build();
+
+        emailVerificationRepository.save(emailVerification);
     }
 }
