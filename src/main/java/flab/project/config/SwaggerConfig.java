053266d6d30c9abec4a66a071103fbf061aa7000
ImWonYong <ssassb123@daum.net>
2023-10-23 17:00:31 +0900

    - SwaggerConfig 추가

---
+++ src/main/java/flab/project/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package flab.project.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Kakaotalk Api")
+                .description("Kakaotalk 백엔드를 클론해보는 F-lab 프로젝트")
+                .version("1.0.0");
+    }
+}
