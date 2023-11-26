36742c266d2940fca770cc26e86c4e55c0daa63e
ImWonYong <ssassb123@daum.net>
2023-11-27 01:27:48 +0900

    feat: 웹소켓 STOMP를 이용한 채팅 만들기
    - 메시지 브로커를 연결
    - kakao-chat으로 웹소켓 연결
    - app 접두사를 사용해 채팅방에 메시지 발행
    - sub 접두사를 사용해 채팅방에 메시지 구독

---
+++ src/main/java/flab/project/config/WebSocketConfig.java
@@ -0,0 +1,27 @@
+package flab.project.config;
+
+import flab.project.handler.UserHandShakeHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/kakao-chat").setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+}
