8280ae8541b7dd311b364507e2a92b12a0e1f159
ImWonYong <ssassb123@daum.net>
2023-11-27 14:25:52 +0900

    feat: 웹소켓 STOMP를 이용한 채팅 만들기
    - ChatParticipantRepository: 두 유저가 속한 개인 채팅방 찾기
    - UserRepository: 유저 정보 가져오기(추후 userId의 친구들을 가져오는 것으로 변경)
    - ChatRoomRepository

--- src/main/java/flab/project/repository/UserRepository.java
+++ src/main/java/flab/project/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package flab.project.repository;
 
 import flab.project.domain.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +10,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    List<User> findAll();
     boolean existsByEmail(String email);
 
     Optional<User> findByPhoneNumber(String phoneNumber);
