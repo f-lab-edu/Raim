a90c136650f66eeca461c7136b85e85d201fa640
ImWonYong <ssassb123@daum.net>
2023-12-04 17:47:20 +0900

    chore: 웹소켓을 이용한 방식으로 변경하며 제거

--- src/main/java/flab/project/controller/ChatController.java
+++ src/main/java/flab/project/controller/ChatController.java
@@ -1,24 +1,9 @@
 package flab.project.controller;
 
-import flab.project.dto.ChatMessageRequestDto;
-import flab.project.service.WebSocketService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
+@RequestMapping("/api/chat")
 public class ChatController {
-    private final WebSocketService webSocketService;
-
-    @MessageMapping("/chat/private-message")
-    public void sendPrivateMessage(ChatMessageRequestDto message) {
-        webSocketService.sendMessageToPrivateRoom(message);
-    }
-
-    @MessageMapping("/chat/group-message")
-    public void sendGroupMessage(ChatMessageRequestDto message) {
-        webSocketService.sendMessageToGroupRoom(message);
-    }
-
 }
