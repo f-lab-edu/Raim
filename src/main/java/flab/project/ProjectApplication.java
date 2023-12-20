f2dfe93e543755462f266ed1b314e1f4012a5354
ImWonYong <ssassb123@daum.net>
2023-12-21 00:02:04 +0900

    feat: JPA Auditing 적용
    - Auditing 사용을 위해 @EnableJpaAuditing 어노테이션 추가
    - BaseEntity 클래스 생성

--- src/main/java/flab/project/ProjectApplication.java
+++ src/main/java/flab/project/ProjectApplication.java
@@ -2,9 +2,11 @@ package flab.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableJpaAuditing
 @EnableScheduling
 public class ProjectApplication {
 
