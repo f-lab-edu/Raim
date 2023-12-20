31999aeeea22e5ab5610295d1ac82985191a34e3
ImWonYong <ssassb123@daum.net>
2023-12-20 18:47:52 +0900

    chore: 코드 수정

--- src/main/java/flab/project/config/WebSocketConfig.java
+++ src/main/java/flab/project/config/WebSocketConfig.java
@@ -1,21 +1,18 @@
 package flab.project.config;
 
 import flab.project.handler.ChatWebSocketHandler;
-import flab.project.interceptor.CustomHandshakeInterceptor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSocket
+@Slf4j
 public class WebSocketConfig implements WebSocketConfigurer {
 
     private final ChatWebSocketHandler chatWebSocketHandler;
@@ -23,8 +20,8 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(chatWebSocketHandler, "/kakao-chat")
-                .addInterceptors(new CustomHandshakeInterceptor())
                 .addInterceptors(new HttpSessionHandshakeInterceptor())
-                .setAllowedOrigins("*");
+                .setAllowedOrigins("*")
+                .setAllowedOriginPatterns("*");
     }
 }
